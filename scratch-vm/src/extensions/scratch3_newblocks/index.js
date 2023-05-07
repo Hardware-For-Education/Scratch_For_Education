483ae10df3833389c1b6ef39c006b1d15f5442c3
Juan Diego Sierra <juandisierra10@gmail.com>
2023-05-06 21:06:28 -0500

    cambios

--- scratch-vm/src/extensions/scratch3_newblocks/index.js
+++ scratch-vm/src/extensions/scratch3_newblocks/index.js
@@ -29,6 +29,12 @@ require("sweetalert");
 
 let the_locale = null;
 
+const FormPlaySoundMotor = {
+    en: "buzzer and vibrator motor [STATE]",
+    es: "zumbador y motor vibrador [STATE]",
+    "es-419": "zumbador y motor vibrador [STATE]",
+};
+
 class Scratch3newblocks {
     constructor(runtime) {
         the_locale = this._setLocale();
@@ -37,12 +43,12 @@ class Scratch3newblocks {
 
     getInfo() {
         the_locale = this._setLocale();
-        this.connect();
+        //this.connect();
 
         return {
             id: "scratch3newblocks",
-            color1: "#0C5986",
-            color2: "#34B0F7",
+            color1: "#7C1212",
+            color2: "#FC8080",
             name: "Scratch New Blocks",
             blocks: [
                 {
@@ -60,8 +66,58 @@ class Scratch3newblocks {
                     },
                 },
             ],
+            menus: {
+                on_off: {
+                acceptReporters: true,
+                items: [
+                    { text: "Encendido", value: "1" },
+                    { text: "Apagado", value: "0" },
+                ],
+            },
+        },
            
         };
+    }
+
+    _setLocale() {
+        let now_locale = "";
+        switch (formatMessage.setup().locale) {
+            case "es-419":
+                now_locale = "es-419";
+                break;
+            case "es":
+                now_locale = "es";
+                break;
+            case "pt-br":
+            case "pt":
+                now_locale = "pt-br";
+                break;
+            case "en":
+                now_locale = "en";
+                break;
+            case "fr":
+                now_locale = "fr";
+                break;
+            case "zh-tw":
+                now_locale = "zh-tw";
+                break;
+            case "zh-cn":
+                now_locale = "zh-cn";
+                break;
+            case "pl":
+                now_locale = "pl";
+                break;
+            case "ja":
+                now_locale = "ja";
+                break;
+            case "de":
+                now_locale = "de";
+                break;
+            default:
+                now_locale = "en";
+                break;
+        }
+        return now_locale;
     }
 }
 
