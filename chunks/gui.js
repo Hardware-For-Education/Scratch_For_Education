46e6209c22cee8952668d04732054e4435e59ef7
Akamiz96 <alejandro.castro2016@outlook.es>
2025-09-22 17:24:43 -0500

    Build for $(git log --pretty=format:%H -n1)

--- chunks/gui.js
+++ chunks/gui.js
@@ -137,7 +137,20 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _lib_hash_parser_hoc_jsx__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../lib/hash-parser-hoc.jsx */ "./src/lib/hash-parser-hoc.jsx");
 /* harmony import */ var _lib_titled_hoc_jsx__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../lib/titled-hoc.jsx */ "./src/lib/titled-hoc.jsx");
 /* harmony import */ var _lib_log_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../lib/log.js */ "./src/lib/log.js");
+/* harmony import */ var scratch_vm__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! scratch-vm */ "./node_modules/scratch-vm/src/index.js");
+/* harmony import */ var scratch_vm__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(scratch_vm__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var scratch_storage__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! scratch-storage */ "./node_modules/scratch-storage/src/index.js");
+/* harmony import */ var scratch_storage__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(scratch_storage__WEBPACK_IMPORTED_MODULE_9__);
+/* harmony import */ var regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! regenerator-runtime/runtime */ "./node_modules/regenerator-runtime/runtime.js");
+/* harmony import */ var regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_10___default = /*#__PURE__*/__webpack_require__.n(regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_10__);
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+
+
 
 
 
@@ -163,6 +176,11 @@ var handleTelemetryModalOptIn = function handleTelemetryModalOptIn() {
 var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
   Object(_lib_log_js__WEBPACK_IMPORTED_MODULE_7__["default"])('User opted out of telemetry');
 };
+
+var getModuloFromURL = function getModuloFromURL() {
+  var p = new URLSearchParams(window.location.search);
+  return p.get('modulo') || 'default.sb3';
+};
 /*
  * Render the GUI playground. This is a separate function because importing anything
  * that instantiates the VM causes unsupported browsers to crash
@@ -170,47 +188,132 @@ var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
  */
 
 
-/* harmony default export */ __webpack_exports__["default"] = (function (appTarget) {
-  _containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__["default"].setAppElement(appTarget); // note that redux's 'compose' function is just being used as a general utility to make
-  // the hierarchy of HOC constructor calls clearer here; it has nothing to do with redux's
-  // ability to compose reducers.
+/* harmony default export */ __webpack_exports__["default"] = (/*#__PURE__*/(function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(appTarget) {
+    var WrappedGui, vm, storage, projectData, modulo, url, resp, buf, _url, _resp, _buf, backpackHostMatches, backpackHost, scratchDesktopMatches, simulateScratchDesktop;
 
-  var WrappedGui = Object(redux__WEBPACK_IMPORTED_MODULE_2__["compose"])(_lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_3__["default"], _lib_hash_parser_hoc_jsx__WEBPACK_IMPORTED_MODULE_5__["default"], _lib_titled_hoc_jsx__WEBPACK_IMPORTED_MODULE_6__["default"])(_containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__["default"]); // TODO a hack for testing the backpack, allow backpack host to be set by url param
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__["default"].setAppElement(appTarget); // note that redux's 'compose' function is just being used as a general utility to make
+            // the hierarchy of HOC constructor calls clearer here; it has nothing to do with redux's
+            // ability to compose reducers.
 
-  var backpackHostMatches = window.location.href.match(/[?&]backpack_host=([^&]*)&?/);
-  var backpackHost = backpackHostMatches ? backpackHostMatches[1] : null;
-  var scratchDesktopMatches = window.location.href.match(/[?&]isScratchDesktop=([^&]+)/);
-  var simulateScratchDesktop;
+            WrappedGui = Object(redux__WEBPACK_IMPORTED_MODULE_2__["compose"])(_lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_3__["default"], _lib_hash_parser_hoc_jsx__WEBPACK_IMPORTED_MODULE_5__["default"], _lib_titled_hoc_jsx__WEBPACK_IMPORTED_MODULE_6__["default"])(_containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__["default"]); // Cargar escenario del modulo
 
-  if (scratchDesktopMatches) {
-    try {
-      // parse 'true' into `true`, 'false' into `false`, etc.
-      simulateScratchDesktop = JSON.parse(scratchDesktopMatches[1]);
-    } catch (_unused) {
-      // it's not JSON so just use the string
-      // note that a typo like "falsy" will be treated as true
-      simulateScratchDesktop = scratchDesktopMatches[1];
-    }
-  }
+            vm = new scratch_vm__WEBPACK_IMPORTED_MODULE_8___default.a();
+            window.scratchVM = vm; // Adjuntar storage mínimo (necesario para cargar .sb3 desde buffer)
 
-  if (false) {}
+            storage = new scratch_storage__WEBPACK_IMPORTED_MODULE_9___default.a();
+            vm.attachStorage(storage);
+            // Descarga del sb3
+            modulo = getModuloFromURL(); // Nombre del modulo en la URL
 
-  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( // important: this is checking whether `simulateScratchDesktop` is truthy, not just defined!
-  simulateScratchDesktop ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
-    isScratchDesktop: true,
-    showTelemetryModal: true,
-    canSave: false,
-    onTelemetryModalCancel: handleTelemetryModalCancel,
-    onTelemetryModalOptIn: handleTelemetryModalOptIn,
-    onTelemetryModalOptOut: handleTelemetryModalOptOut
-  }) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
-    backpackVisible: true,
-    showComingSoon: true,
-    backpackHost: backpackHost,
-    canSave: false,
-    onClickLogo: onClickLogo
-  }), appTarget);
-});
+            url = "https://tinycoderspuj.github.io/TinyCodersModules/".concat(modulo);
+            console.log('📦 Descargando proyecto:', url);
+            _context.prev = 9;
+            _context.next = 12;
+            return fetch(url);
+
+          case 12:
+            resp = _context.sent;
+
+            if (resp.ok) {
+              _context.next = 15;
+              break;
+            }
+
+            throw new Error("HTTP ".concat(resp.status));
+
+          case 15:
+            _context.next = 17;
+            return resp.arrayBuffer();
+
+          case 17:
+            buf = _context.sent;
+            projectData = new Uint8Array(buf);
+            console.log('✅ .sb3 descargado');
+            _context.next = 35;
+            break;
+
+          case 22:
+            _context.prev = 22;
+            _context.t0 = _context["catch"](9);
+            console.error('❌ No se pudo descargar el .sb3:', _context.t0);
+            _url = "https://tinycoderspuj.github.io/TinyCodersModules/default.sb3";
+            _context.next = 28;
+            return fetch(_url);
+
+          case 28:
+            _resp = _context.sent;
+
+            if (_resp.ok) {
+              _context.next = 31;
+              break;
+            }
+
+            throw new Error("HTTP ".concat(_resp.status));
+
+          case 31:
+            _context.next = 33;
+            return _resp.arrayBuffer();
+
+          case 33:
+            _buf = _context.sent;
+            projectData = new Uint8Array(_buf);
+
+          case 35:
+            // TODO a hack for testing the backpack, allow backpack host to be set by url param
+            backpackHostMatches = window.location.href.match(/[?&]backpack_host=([^&]*)&?/);
+            backpackHost = backpackHostMatches ? backpackHostMatches[1] : null;
+            scratchDesktopMatches = window.location.href.match(/[?&]isScratchDesktop=([^&]+)/);
+
+            if (scratchDesktopMatches) {
+              try {
+                // parse 'true' into `true`, 'false' into `false`, etc.
+                simulateScratchDesktop = JSON.parse(scratchDesktopMatches[1]);
+              } catch (_unused) {
+                // it's not JSON so just use the string
+                // note that a typo like "falsy" will be treated as true
+                simulateScratchDesktop = scratchDesktopMatches[1];
+              }
+            }
+
+            if (false) {}
+
+            react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( // important: this is checking whether `simulateScratchDesktop` is truthy, not just defined!
+            simulateScratchDesktop ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
+              isScratchDesktop: true,
+              showTelemetryModal: true,
+              canSave: false,
+              onTelemetryModalCancel: handleTelemetryModalCancel,
+              onTelemetryModalOptIn: handleTelemetryModalOptIn,
+              onTelemetryModalOptOut: handleTelemetryModalOptOut,
+              vm: vm,
+              projectData: projectData
+            }) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
+              backpackVisible: true,
+              showComingSoon: true,
+              backpackHost: backpackHost,
+              canSave: false,
+              onClickLogo: onClickLogo,
+              vm: vm,
+              projectData: projectData
+            }), appTarget);
+
+          case 41:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[9, 22]]);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+})());
 
 /***/ }),
 
